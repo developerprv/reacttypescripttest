050b3975fcf7be8b5769ac1a70b1286a345c32fa
PRV Developer <developer@BTG.Local>
2022-03-10 09:39:44 -0500

    see App.tsx for explanation and Test.js

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // auto import/suggestion shows the correct import from react
+  const [test, setTest] = useState();
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/Test.js
@@ -0,0 +1,5 @@
+function Test() {
+  // The auto import/suggestion shows correct even in a .js file
+  const [test, setTest] = useState();
+  return(<div>Test Component</div>);
+}
